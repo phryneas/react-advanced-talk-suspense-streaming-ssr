c1a1e3327ef9dffa08f40f09c606716c90d6dec5
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2023-10-16 18:03:24 +0200

    wording

--- src/app/19_it_works_well/page.tsx
+++ src/app/19_it_works_well/page.tsx
@@ -12,7 +12,7 @@ export default function Page() {
       </header>
       <footer className="light">
         <p>
-          I&apos;d actually love to call it &quot;stable&quot; at this point,
+          I&apos;d actually call it &quot;stable&quot; at this point,
           <br />
           but timing isn&apos;t where I&apos;d love it to be.
           <br />
